5b1757ab2fbe65de9b19c4ba60f0a38b8fcdd661
Morgan Sterling <git.acct01@gmail.com>
2025-02-12 09:15:17 +0800

    Implemented auto gas optimization for Hardhat environment

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,9 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
     solidity: "0.8.0",
     networks: {
-        hardhat: {},
+        hardhat: {
+            gas: "auto" // Added to simulate automatic gas optimization
+        },
         localhost: {
             url: "http://127.0.0.1:8545"
         }
